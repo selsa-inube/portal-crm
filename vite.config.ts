80cd5c719cdf025f5bd4203b644c0041b4a358f0
carin <ca.rincon97@gmail.com>
2024-07-22 09:30:34 -0500

    feat: implementing husky, conventional commit and prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import vitesconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), vitesconfigPaths()],
+  server: {
+    open: true,
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@src": path.resolve(__dirname, "./src"),
+      "@design": path.resolve(__dirname, "./src/design"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@config": path.resolve(__dirname, "./src/config"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@mocks": path.resolve(__dirname, "./src/mocks"),
+      "@ptypes": path.resolve(__dirname, "./src/types"),
+      "@forms": path.resolve(__dirname, "./src/forms"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@services": path.resolve(__dirname, "./src/services"),
+    },
+  },
+});
