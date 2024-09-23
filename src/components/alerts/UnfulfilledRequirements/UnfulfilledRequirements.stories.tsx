e346b7933b16a2e2c080e29cd275efb338d3a0fa
Andres Babativa <carlos3k11@gmail.com>
2024-09-23 13:26:51 -0500

    feat: add UnfulfilledRequirements component and its styles

---
+++ src/components/alerts/UnfulfilledRequirements/UnfulfilledRequirements.stories.tsx
@@ -0,0 +1,39 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { UnfulfilledRequirements } from ".";
+
+type Story = StoryObj<typeof UnfulfilledRequirements>;
+
+const meta: Meta<typeof UnfulfilledRequirements> = {
+  component: UnfulfilledRequirements,
+  title: "components/alerts/UnfulfilledRequirements",
+  argTypes: {
+    title: {
+      control: {
+        type: "text",
+      },
+      description: "title of the component",
+    },
+    requirement: {
+      control: {
+        type: "text",
+      },
+      description: "requirement of the component",
+    },
+    causeNoncompliance: {
+      control: {
+        type: "text",
+      },
+      description: "causeNoncompliance of the component",
+    },
+  },
+};
+
+export const Default: Story = {
+  args: {
+    title: "Alerta",
+    requirement: "Requiere 90 días de antigüedad.",
+    causeNoncompliance: "El cliente tine solo 60 días de afiliación.",
+  },
+};
+
+export default meta;
---
+++ src/components/alerts/UnfulfilledRequirements/index.tsx
@@ -0,0 +1,42 @@
+import { MdWarningAmber } from "react-icons/md";
+import { Divider } from "@inubekit/divider";
+import { Icon } from "@inubekit/icon";
+import { Stack } from "@inubekit/stack";
+import { Text } from "@inubekit/text";
+
+import { Fieldset } from "@src/components/data/Fieldset";
+
+interface IUnfulfilledRequirementsProps {
+  title: string;
+  requirement: string;
+  causeNoncompliance: string;
+}
+
+export const UnfulfilledRequirements = (
+  props: IUnfulfilledRequirementsProps
+) => {
+  const { title, requirement, causeNoncompliance } = props;
+  return (
+    <Fieldset title={title}>
+      <Stack direction="column" gap="16px">
+        <Stack direction="column" gap="4px">
+          <Stack justifyContent="space-between" alignItems="center">
+            <Text>Requisito:</Text>
+            <Icon icon={<MdWarningAmber />} appearance="warning" size="24px" />
+          </Stack>
+          <Divider />
+          <Text appearance="gray" size="medium">
+            {requirement}
+          </Text>
+        </Stack>
+        <Stack direction="column" gap="4px">
+          <Text>Causa de incumplimiento:</Text>
+          <Divider />
+          <Text appearance="gray" size="medium">
+            {causeNoncompliance}.
+          </Text>
+        </Stack>
+      </Stack>
+    </Fieldset>
+  );
+};
