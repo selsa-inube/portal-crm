47bc53dc4a84a76734316402494e4e9e6f5ec68a
Andres Babativa <carlos3k11@gmail.com>
2024-09-27 14:56:52 -0500

    feat: organise  import

--- src/components/layout/AppPage/index.tsx
+++ src/components/layout/AppPage/index.tsx
@@ -1,3 +1,4 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Outlet } from "react-router-dom";
 import { Grid } from "@inubekit/grid";
 import { Header } from "@inubekit/header";
@@ -14,7 +15,6 @@ import {
   StyledMain,
 } from "./styles";
 import linparLogo from "@assets/images/linpar.png";
-import { useAuth0 } from "@auth0/auth0-react";
 
 const renderLogo = (imgUrl: string) => {
   return (
